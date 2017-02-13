ec0c6af5b5030a0ce4594718cd5a6ffdb7264711
Aaron Snowberger <jekkilekki@gmail.com>
2017-02-13 12:31:58 +0900

    Gallery frontend functional

--- class.spacex-gallery.php
+++ class.spacex-gallery.php
@@ -9,23 +9,10 @@ if ( ! class_exists( 'Space_X_Gallery' ) ) {
     
 class Space_X_Gallery {
         
-        /**
-         * Message to display in the admin_notice
-         * @var string
-         */
-        public $message = '';
-        
-        /**
-         * Error to display in the admin_notice
-         * @var string
-         */
-        public $error = '';
-        
         private $name;
         private $version;
-        private $shortcode;
         
-        /** NEW
+        /**
          * Stores the class instance.
          * 
          * @var Space_X_Gallery
@@ -49,6 +36,8 @@ class Space_X_Gallery {
          */
         public function init_plugin() {
             $this->init_hooks();
+            // Apply filter to default gallery shortcode
+            //add_filter( 'post_gallery', array( $this::get_instance(), 'spacex_post_gallery' ), 10, 2 );
         }
         
         /**
@@ -56,15 +45,7 @@ class Space_X_Gallery {
          *  - admin_print_scripts
          */
         private function init_hooks() {
-            add_action( 'wp_enqueue_media', array( $this, 'spacex_add_media_option' ) );
-            add_action( 'print_media_templates', array( $this, 'spacex_media_templates' ) );
-        }
-                
-        /**
-         * Constructor. Initializes WordPress hooks
-         */
-        public function __construct() {
-            add_action( 'wp_enqueue_media', array( $this, 'spacex_add_media_option' ) );
+            add_action( 'wp_enqueue_media', array( $this, 'spacex_add_media_settings' ) );
             add_action( 'print_media_templates', array( $this, 'spacex_media_templates' ) );
         }
         
@@ -80,18 +61,14 @@ class Space_X_Gallery {
         /**
          * Media UI integration (Enqueues the script)
          */
-        public function spacex_add_media_option() {
-            //add_action( 'admin_print_footer_scripts', array( $this, 'spacex_media_gallery_option' ) );
-        
-            /** NEW
-             * From Dominik Shilling
-             */
-            if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' )
-                return;
+        public function spacex_add_media_settings() {
+
+//            if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' )
+//                return;
             
             wp_enqueue_script(
-                    'spacex-gallery-setting',
-                    plugins_url( 'js/spacex-gallery-setting.js', __FILE__ ),
+                    'spacex-gallery-settings',
+                    plugins_url( 'js/spacex-gallery-settings.js', __FILE__ ),
                     array( 'media-views' )
             );
             
@@ -100,70 +77,223 @@ class Space_X_Gallery {
         /**
          * Outputs the view template with the custom setting.
          */
-        public function spacex_media_gallery_option() {
-            
-            if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' )
-                return;
-            
-            ?>
-            <!--<script type="text/javascript">-->
-//            jQuery( function() {
-//                if( wp.media.view.Settings.Gallery ) {
-//                    wp.media.view.Settings.Gallery = wp.media.view.Settings.extend({
-//                       className: 'collection-settings gallery-settings',
-//                       template: wp.media.template( 'gallery-settings' ),
-//                       render: function() {
-//                           wp.media.View.prototype.render.apply( this, arguments );
-//                           // Append our option
-//                           var $s = this.$('select.size');
-//                           //if( !$s.find( 'option[value="0"]' ).length ) {
-//                               $s.append( '<label>SpaceX-ify!!!</label>' );
-//                           //}
-//                           // Select the correct values.
-//                           _( this.model.attributes ).chain().keys().each( this.update, this );
-//                           return this;
-//                       }
-//                    });
-//                }
-//            });
-            <!--</script>-->
-            
-            <script type="text/html" id="tmpl-custom-gallery-setting">
-                <label class="setting">
-                    <span>Type</span>
-                    <select class="type" name="type" data-setting="type">
-                        <?php
-                        
-                        $types = array(
-                            'default'   => __( 'SpaceX', 'spacex-gallery' )
-                        );
-                        
-                        foreach ( $types as $value => $name ) : ?>
-                            <option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, 'default' ); ?>>
-                                <?php echo esc_html( $name ); ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </label>
-            </script>
-            
-            <?php
-        }
-        
         public function spacex_media_templates() {
             if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' )
                 return;
 
             ?>
             <script type="text/html" id="tmpl-spacex-gallery-setting">
+                <h2 style="margin-top: 1em; display: inline-block;"><br><?php _e( 'SpaceX Gallery Settings', 'spacex-gallery' ); ?></h2>
+                
                 <label class="setting">
-                    <span><?php _e( 'SpaceX Gallery', 'spacex-gallery' ); ?></span>
-                    <input class="check-spacex" type="checkbox" name="check-spacex" data-setting="check-spacex">
+                    <span><?php _e( 'Create SpaceX Gallery?', 'spacex-gallery' ); ?></span>
+                    <input class="spacex" type="checkbox" name="spacex" data-setting="spacex">
                 </label>
+                
+                <label class="setting">
+                    <span><?php _e( 'Set small row speed (ms)', 'spacex-gallery' ); ?></span>
+                    <input class="smallspeed" type="text" name="smallspeed" style="float:left;" data-setting="smallspeed" placeholder="50000">
+                </label>
+                
+                <label class="setting">
+                    <span><?php _e( 'Set Large row speed (ms)', 'spacex-gallery' ); ?></span>
+                    <input class="largespeed" type="text" name="largespeed" style="float:left;" data-setting="largespeed" placeholder="90000">
+                </label>
+                
+                <p style="color: #666; display: inline-block; font-style: italic">
+                    <?php _e( 'Image sizes: The SpaceX Gallery creates 2 rows of images. The top row is smaller and automatically pulls `medium` images from the database. The bottom row is larger and pulls `large` images from the database.', 'spacex-gallery' ); ?>
+                    <br>
+                    <?php _e( 'Speed: Enter speeds in milliseconds.', 'spacex-gallery' ); ?>
+                </p>
             </script>
             <?php
         }
-
-}
+        
+        /**
+         * The SpaceX Gallery Shortcode
+         * 
+         * This implements the functionality of the Gallery Shortcode 
+         * for displaying WordPress images in a post.
+         * 
+         * @since 0.0.1
+         * 
+         * @param array $atts Attributes of the shortcode.
+         * @return string HTML content to display gallery.
+         */
+        public function spacex_post_gallery( $output, $attr ) {
+            
+            // Initialize
+            global $post, $wp_locale;
+            $return = $output; // fallback
+            
+            // Gallery instance counter
+            static $instance = 0;
+            $instance++;
+            
+            // Validate the author's orderby attribute
+            if ( isset( $attr['orderby'] ) ) {
+                $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+                if( ! $attr['orderby'] ) unset( $attr['orderby'] );
+            }
+            
+            // Get attributes from shortcode
+            extract( shortcode_atts( array(
+                'order'         => 'ASC',
+                'orderby'       => 'menu_order ID',
+                'id'            => $post->ID,
+                'itemtag'       => 'dl',
+                'icontag'       => 'dt',
+                'captiontag'    => 'dd',
+                'captions'      => 'onhover',
+                'captiontype'   => 'p',
+                'columns'       => 3,
+                'gutterwidth'   => '10',
+                'link'          => 'post',
+                'size'          => 'medium',
+                'spacex'        => 'true',
+                'smallspeed'    => 50000,
+                'largespeed'    => 90000,
+                'include'       => '',
+                'exclude'       => ''
+            ), $attr ) );
+            
+            // Initialize
+            $id = intval( $id );
+            $attachments = array();
+            if ( 'RAND' == $order ) $orderby = 'none';
+            
+            if ( ! empty( $include ) ) {
+                
+                // If include attribute is present
+                $include = preg_replace( '/[^0-9,]+/', '', $include );
+                $_attachments = get_posts( array( 
+                    'include' => $include, 
+                    'post_status' => 'inherit', 
+                    'post_type' => 'attachment',
+                    'post_mime_type' => 'image',
+                    'order' => $order,
+                    'orderby' => $orderby
+                ) );
+                
+                // Setup attachments array
+                foreach ( $_attachments as $key => $val ) {
+                    $attachments[ $val->ID ] = $_attachments[ $key ];
+                }
+                
+            } else if ( ! empty( $exclude ) ) {
+                
+                // If exclude attribute is present
+                $exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+                
+                // Setup attachments array
+                $attachments = get_children( array( 
+                    'post_parent' => $id,
+                    'exclude'   => $exclude,
+                    'post_status' => 'inherit', 
+                    'post_type' => 'attachment',
+                    'post_mime_type' => 'image',
+                    'order' => $order,
+                    'orderby' => $orderby
+                ));
+                
+            } else {
+                
+                // If no include nor exclude attributes, setup attachments array
+                $attachments = get_children( array( 
+                    'post_parent' => $id,
+                    'post_status' => 'inherit', 
+                    'post_type' => 'attachment',
+                    'post_mime_type' => 'image',
+                    'order' => $order,
+                    'orderby' => $orderby
+                ));
+                
+            }
+            
+            if ( empty( $attachments ) ) return '';
+            
+            // Filter gallery differently for feeds
+            if ( is_feed() ) {
+                $output = "\n";
+                foreach ( $attachments as $att_id => $attachment ) $output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+                return $output;
+            }
+            
+            // Filter tags and attributes
+            $itemtag = tag_escape( $itemtag );
+            $captiontag = tag_escape( $captiontag );
+            $columns = intval( $columns );
+            $itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+            $float = is_rtl() ? 'right' : 'left';
+            $selector = "gallery-{$instance}";
+            
+            // Filter gallery CSS
+            $output = apply_filters( 'gallery_style', "
+                <style type='text/css'>
+                    #{$selector} {
+                        margin: auto;
+                    }
+                    #{$selector} .gallery-item {
+                        float: {$float};
+                        margin-top: 10px;
+                        text-align: center;
+                        width: {$itemwidth}%;
+                    }
+                    #{$selector} img {
+                        border: 2px solid #cfcfcf;
+                    }
+                    #{$selector} .gallery-caption {
+                        margin-left: 0;
+                    }
+                </style>
+                <!-- see gallery_shortcode() in wp-includes/media.php -->
+                <div id='$selector' class='gallery galleryid-{$id}'>"
+            );
+            
+            //$images = get_posts( $args );
+            
+            // Iterate through the attachments in this gallery instance
+            $i = 0;
+            foreach ( $attachments as $id => $attachment ) {
+                
+                // Attachment link
+                $link = isset( $attr['link'] ) && 'file' == $attr['link'] ? wp_get_attachment_link( $id, $size, false, false ) : wp_get_attachment_link( $id, $size, true, false );
+                
+                // Begin itemtag
+                $output .= "<{$itemtag} class='gallery-item'>";
+                
+                // icontag
+                $output .= "
+                <{$icontag} class='gallery-icon'>
+                    $link 
+                </{$icontag}>";
+                
+                if ( $captiontag && trim( $attachment->post_excerpt ) ) {
+                    
+                    // captiontag
+                    $output .= "
+                    <{$captiontag} class='gallery-caption'>
+                        " . wptexturize( $attachment->post_excerpt ) . "
+                    </{$captiontag}";
+                    
+                }
+                
+                // End itemtag
+                $output .= "</{$itemtag}>";
+                
+                // Line breaks by columns set
+                if ( $columns > 0 && ++$i % $columns == 0 ) $output .= '<br style="clear: both;">';
+                    
+            }
+            
+            // End gallery output
+            $output .= "
+                <br style='clear: both;'>
+            </div>\n";
+            
+            return $output;
+        }
+        
+} // END Space_X_Gallery
 
 } // END if ( ! class_exists() )
--- js/spacex-gallery-settings.js
+++ js/spacex-gallery-settings.js
@@ -1,7 +1,12 @@
 /**
- * Custom Gallery Setting
+ * SpaceX Gallery Settings
  */
 ( function( $ ) {
+    "use strict";
+    
+    if ( 'undefined' == typeof( wp.media ) ) 
+        return;
+    
     var media = wp.media;
     
     // Wrap the render() function to append controls
@@ -13,8 +18,13 @@
             this.$el.append( media.template( 'spacex-gallery-setting' ) );
             
             // Save the setting
-            media.gallery.defaults.size = 'default';
-            this.update.apply( this, ['type'] );
+            media.gallery.defaults.spacex = true;
+            media.gallery.defaults.smallspeed = '50000';
+            media.gallery.defaults.largespeed = '90000';
+            this.update.apply( this, ['spacex'] );
+            this.update.apply( this, ['smallspeed'] );
+            this.update.apply( this, ['largespeed'] );
+            
             return this;
         }
     });
